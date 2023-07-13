229153eefc9e86939bd3b51ad10538850fe0a217
slbin-park <smpts00@naver.com>
2023-07-13 21:32:38 +0900

    JPA repository 생성

--- jpastudy/src/main/java/com/example/jpastudy/repository/MemberRepository.java
+++ jpastudy/src/main/java/com/example/jpastudy/repository/MemberRepository.java
@@ -1,4 +1,7 @@
 package com.example.jpastudy.repository;
 
-public class MemberRepository {
+import com.example.jpastudy.repository.entity.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MemberRepository extends JpaRepository<Member,Long> {
 }
